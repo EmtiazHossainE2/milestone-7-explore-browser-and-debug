ac93070ddd5a689edd190054a72298f5aa17a92a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-03 20:09:17 +0600

    fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,12 @@
-// setTimeout(() => console.log("I am Superman"), 1000)
-// console.log("I am Superwoman");
+setTimeout(() => console.log("I am Superman"), 1000)
+console.log("I am Superwoman");
+
+console.log(111111);
+console.log(222222);
+setTimeout(() => console.log('aaaaaaa'), 5000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+console.log(333333);
+console.log(444444);
+console.log(77777);
