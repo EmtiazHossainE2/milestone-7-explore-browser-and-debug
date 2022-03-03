fbef9d5f163ac7f81ef9390844cb8bcff7df4d3e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-03 20:55:27 +0600

    alert msg

---
+++ browser-api/js/alert.js
@@ -0,0 +1,27 @@
+console.log('linked');
+
+// alert('Welcome buddy !!! ')
+const alertMsg = () => {
+    alert('why are you here ??? go to sleep .');
+}
+const goingTour = () => {
+    const result = confirm('Are you interest to go with us ? ')
+    console.log(result);
+    if (result === true) {
+        alert('tk Bkash kore de ')
+    }
+    else {
+        alert('dureee giya mor !!!!')
+    }
+}
+
+const sendMoney = () => {
+    const money = prompt('Send Money 5000 for confirm your tour list')
+    if (money === 5000) {
+        alert('okay your name listed')
+        console.log('get 5000 tk');
+    }
+    else {
+        alert('hala dure gia mor.ulta palta tk dish kn')
+    }
+}
