06325d13f114912095ef961aff4ad5fa0e6cdaa5
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-03 11:13:15 +0600

    asynchronous js basic idea

---
+++ js/asynchronous.js
@@ -0,0 +1,12 @@
+const mySelf = () => {
+    console.log(`My name is Emtiaz Hossain Emon. I'm learning JavaScript`);
+}
+console.log("First : I am first ");
+console.log(`Second : I'm second `);
+console.log(`Third : I'm Thrid`);
+setTimeout(mySelf, 5000)
+setTimeout(function () {
+    console.log('JavaScript is awesome');
+}, 4000)
+// mySelf()
+console.log(`Fourth : I'm Fourth`);
