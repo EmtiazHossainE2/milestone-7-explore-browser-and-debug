da2ed89c61caac2bb0be94308fc71a81e9aef76b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-06 00:23:38 +0600

    fixed all error homework by mir vai

--- miujik-error/script.js
+++ miujik-error/script.js
@@ -4,10 +4,16 @@ const elementById = (id) => {
 
 const handleSearch = () => {
   const keyword = elementById("keyword");
+  const artistContainer = elementById("artists");
+  const albumContainer = elementById("albums");
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
+
+  keyword.value = ''
+  artistContainer.innerHTML = ''
+  albumContainer.innerHTML = ''
 };
 
 const showArtists = (data) => {
