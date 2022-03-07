db3358edab16cd4379643b468a35f12cada196c6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-07 21:13:15 +0600

    display product local storage to cart

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,9 +1,15 @@
 const addBtn = () => {
     const inputValue = document.getElementById('input-value')
     const items = inputValue.value;
+    if (!items) {
+        return
+    }
 
     //display items 
     displayItems(items)
+    //add product to cart 
+    addProductToCart(items)
+
     inputValue.value = ''
 }
 const displayItems = items => {
@@ -12,3 +18,31 @@ const displayItems = items => {
     li.innerText = items;
     ul.appendChild(li)
 }
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart')
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart)
+    }
+    else {
+        cartObj = {}
+    }
+    return cartObj
+}
+
+const addProductToCart = items => {
+    const cart = getCart()
+    cart[items] = 1
+    // console.log(cart);
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('cart', cartStringified)
+
+}
+const displayLocatStorageCart = () => {
+    const cart = getCart()
+    for (const items in cart) {
+        displayItems(items)
+    }
+}
+displayLocatStorageCart()
