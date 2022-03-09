a903eb70254876842a2b6a21e05d2db8ddcbc1ad
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-09 18:32:29 +0600

    strike through

--- debug-final-checklist/Issue-tracker/main.js
+++ debug-final-checklist/Issue-tracker/main.js
@@ -1,3 +1,14 @@
+const addValue = () => {
+  const issueValue = document.getElementById('tracker-value')
+
+  console.log(parseInt(issueValue.innerText));
+  // const totalIssue = parseInt(issueValue.innerText) + 1
+  // console.log(totalIssue);
+  const issuValue = localStorage.getItem('issues')
+  const parseIssue = JSON.parse(issuValue)
+  issueValue.innerText = parseIssue.length
+}
+
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
 
 function submitIssue(e) {
@@ -46,14 +57,17 @@ const fetchIssues = () => {
   for (var i = 0; i < issues.length; i++) {
     const { id, description, severity, assignedTo, status } = issues[i];
 
-    issuesList.innerHTML += `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
-                              </div>`;
+    issuesList.innerHTML += `
+    <div class="well">
+        <h6>Issue ID: ${id} </h6>
+        <p><span class="label label-info"> ${status} </span></p>
+        <h3 class="${status === 'Open' ? '' : 'strikeThrough'}"> ${description} </h3>
+        <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+        <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+        <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">${status === 'Open' ? 'Close' : 'Close'}
+        </a >
+    <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
+    </div >
+  `;
   }
 }
